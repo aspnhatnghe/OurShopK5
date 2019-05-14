21696f39204216424e119e4c5783f07447d8c9e0
NN <NN@PC15>
2019-05-14 21:15:51 +0700

    Day01 - Layout, simple model

---
+++ OurShopK5/DataModels/HangHoa.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OurShopK5.DataModels
+{
+    [Table("HangHoa")]
+    public class HangHoa
+    {
+        [Key]
+        public int MaHh { get; set; }
+        public string TenHh { get; set; }
+        public string Hinh { get; set; }
+        public string MoTa { get; set; }
+        public double DonGia { get; set; }
+        public byte GiamGia { get; set; }
+        public int MaLoai { get; set; }
+        [ForeignKey("MaLoai")]
+        public Loai Loai { get; set; }
+    }
+
+    public class QuaTang
+    {
+        public int QuaTangId { get; set; }
+        public string TenQua { get; set; }
+        public string MoTa { get; set; }        
+        public string Code { get; set; }
+        public DateTime HanDung { get; set; }
+    }
+
+    public class NhanQua
+    {
+        public int Id { get; set; }
+        public int MaQT { get; set; }
+        public int MaHh { get; set; }
+        public string MaKh { get; set; }
+        public DateTime? NgayNhan { get; set; }
+        [ForeignKey("MaQT")]
+        public QuaTang QuaTang { get; set; }
+        [ForeignKey("MaHh")]
+        public HangHoa HangHoa { get; set; }
+    }
+}
---
+++ OurShopK5/DataModels/Loai.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OurShopK5.DataModels
+{
+    [Table("Loai")]
+    public class Loai
+    {
+        [Key]
+        [Display(Name ="Mã loại")]
+        public int MaLoai { get; set; }
+        [Display(Name = "Tên loại")]
+        public string TenLoai { get; set; }
+        [Display(Name = "Mô tả")]
+        public string MoTa { get; set; }
+        [Display(Name = "Hình")]
+        public string Hinh { get; set; }
+
+        public int MaLoaiCha { get; set; }
+    }
+}
