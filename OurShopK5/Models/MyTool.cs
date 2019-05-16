55edf05566c5a8f9febfdcf2fe6d8ac33809a844
NN <NN@PC15>
2019-05-16 21:12:04 +0700

    HangHoa backend

---
+++ OurShopK5/Models/MyTool.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OurShopK5.Models
+{
+    public class MyTool
+    {
+        public static string XuLyUpload(IFormFile fHinh, string thuMuc)
+        {
+            string fileName = "";
+            if (fHinh != null)
+            {
+                string fullPath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "Hinh", thuMuc, fHinh.FileName);
+                using (var file = new FileStream(fullPath, FileMode.Create))
+                {
+                    fHinh.CopyTo(file);
+                    fileName = fHinh.FileName;
+                }
+            }
+
+            return fileName;
+        }
+    }
+}
--- OurShopK5/Startup.cs
+++ OurShopK5/Startup.cs
@@ -58,6 +58,10 @@ namespace OurShopK5
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
+                    name: "areas",
+                    template: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
+                routes.MapRoute(
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
